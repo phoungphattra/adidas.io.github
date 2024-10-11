790a27d0c0e9078166de22d7109f315b9a6bdd2f
phoungphattra <phoungchanphattra737@gmail.com>
2024-10-10 23:18:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
